140acc51ac991042740ba47cba09878ff3e5fe26
Akhil Parekh <parekhakhil11@gmail.com>
2022-09-17 14:43:16 +0530

    initialized models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Device(models.Model):
+    device_fk = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.device_fk)
+
+
+class DeviceData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    latitude = models.DecimalField(max_digits=6, decimal_places=3)
+    longitude = models.DecimalField(max_digits=6, decimal_places=3)
+    time_stamp = models.DateTimeField()
+    sts = models.DateTimeField()
+    speed = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.device.device_fk)
--- carnot_task/settings.py
+++ carnot_task/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,18 @@ DATABASES = {
     }
 }
 
+CACHE_TTL = 3600
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -111,11 +125,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
